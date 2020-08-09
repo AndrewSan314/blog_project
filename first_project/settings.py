a7e690b4d7d63d4e2687e3f35f21836db1d3e0f8
AndrewSan314 <taduchien314@gmail.com>
2020-08-09 15:16:20 +0700

    first_commit

--- first_project/settings.py
+++ first_project/settings.py
@@ -120,8 +120,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = '/login/'
 
 LOGIN_REDIRECT_URL = '/'
